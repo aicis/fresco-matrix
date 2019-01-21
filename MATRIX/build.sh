93693035e488672a579b81e51f3ef64e7180e644
Peter Sebastian Nordholt <peter.s.nordholt@alexandra.dk>
2019-01-21 16:01:39 +0100

    remembering sudo.

--- MATRIX/build.sh
+++ MATRIX/build.sh
@@ -3,6 +3,6 @@ BASEDIR=..
 cd $BASEDIR
 if [ -n "$(command -v apt-get)" ] 
 then
-   sudo apt-get update && apt-get install -y openjdk-11-jdk maven
+   sudo apt-get update && sudo apt-get install -y openjdk-11-jdk maven
 fi
 mvn clean package -DskipTests
